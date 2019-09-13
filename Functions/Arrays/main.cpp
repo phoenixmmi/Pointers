deea966b52bd04bc2767e4baac3055fe498d972c
Axel <Axel@201-8>
2019-09-13 19:28:39 +0300

    Function overloading start.

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 void FillRand(int Arr[], const int n);
+void FillRand(double Arr[], const int n);
 void Print(int Arr[], const int n);
+void Print(double Arr[], const int n);
 void Sort(int Arr[], const int n);
+int  Sum(int Arr[], const int n);
+double Avg(int Arr[], const int n);
+int  minValueIn(int Arr[], const int n);
 
 void main()
 {
@@ -13,9 +18,13 @@ void main()
 
 	FillRand(Arr, n);
 	Print(Arr, n);
-	Sort(Arr, n);
-	Print(Arr, n);
-	
+	cout << "Сумма элементов масива:\t" << Sum(Arr, n) << endl;
+	cout << "Среднее арифметическое:\t" << Avg(Arr, n) << endl;
+	//cout << "Минимальное значение в массиве:\t" << minValueIn(Arr, n) << endl;
+
+	double Brr[n];
+	FillRand(Brr, n);
+	Print(Brr, n);
 }
 
 void FillRand(int Arr[], const int n)
@@ -26,8 +35,26 @@ void FillRand(int Arr[], const int n)
 		Arr[i] = rand() % 100;
 	}
 }
+void FillRand(double Arr[], const int n)
+{
+	//Заполнение случайными числами:
+	for (int i = 0; i < n; i++)
+	{
+		Arr[i] = rand() % 10000;
+		Arr[i] /= 100;
+	}
+}
 
 void Print(int Arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << Arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(double Arr[], const int n)
 {
 	//Вывод массива на экран:
 	for (int i = 0; i < n; i++)
@@ -53,3 +80,24 @@ void Sort(int Arr[], const int n)
 		}
 	}
 }
+
+int  Sum(int Arr[], const int n)
+{
+	int Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += Arr[i];
+	}
+	return Sum;
+}
+
+double Avg(int Arr[], const int n)
+{
+	return (double)Sum(Arr, n) / n;
+}
+
+//int  minValueIn(int Arr[], const int n)
+//{
+//	int min = Arr[0];
+//
+//}
