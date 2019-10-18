a5957b8f0e6267f134bc4f089a6a332fb6229294
Axel <Axel@201-8>
2019-10-18 20:53:24 +0300

    Pointer start.

---
+++ Pointers/Pointers/main.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+//#define POINTERS_BASICS
+//#define DECLARATION_OF_POINTERS
+#define POINTERS_AND_ARRAYS
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef POINTERS_BASICS
+	int a = 2;
+	int *pa = &a;//¬енгерской нотации
+	//pa - pointer to a.
+	//& - оператор вз€ти€ адреса (Address-of operator)
+	cout << a << endl;
+	cout << &a << endl;
+	cout << pa << endl;
+	cout << *pa << endl;
+	//* - ќператор разыменовани€ (Dereference operator).
+	//*
+	int *pb;
+	int b = 3;
+	pb = &b;
+	b;//(int)
+	pb;//(int*)
+	cout << pb << endl;
+	cout << *pb << endl;
+	//int - int
+	//int* - указатель на int.
+	//double - double.
+	//double* - указатель на double.  
+#endif // POINTERS_BASICS
+
+#ifdef DECLARATION_OF_POINTERS
+	int a, b, c;	//ќбъ€вление трех переменных одним выражением.
+	int *pa, *pb, *pc;	//ќбъ€вление трех указателей одним выражением.
+	int* pd, pe, pf;	//ќбъ€витс€ один указатель на int (pd), 
+						//и две переменные типа int (pe и pf).
+#endif
+
+	const int n = 5;
+	short Arr[n] = { 3, 5, 8, 13, 21 };
+	cout << *Arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(Arr + i) << "\t";
+	}
+	cout << endl;
+	//+, -, ++, --;
+}
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,19 @@
-Добавить ничью.
+Pointers
+http://www.cplusplus.com/doc/tutorial/pointers/
+x86 - 4B address
+x86-64 - 8B address
+
+0x00 - 1B
+0x0000 - 2B
+0x00000000 - 4B
+0x00000000 00000000 - 8B
+
+1 Byte  = 8 bit;
+1 kByte = 1024 B  = 2^10 B;
+1 MByte = 1024 kB = 2^20 B;
+1 GByte = 1024 MB = 2^30 B;
+1 TByte = 1024 GB = 2^40 B;
+1 PByte = 1024 TB = 2^50 B;
+1 EByte = 1024 PB = 2^60 B;
+
+2^64 B = 2^60B*2^4 = 16*1EB = 16EB;
