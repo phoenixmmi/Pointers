eb41cda79cbd13af976ef5bcfba776862c75be48
Student <Student@201-4>
2019-11-25 20:58:36 +0200

    CW 25.11

--- NullTerminatedLines/NullTerminatedLines/main.cpp
+++ NullTerminatedLines/NullTerminatedLines/main.cpp
@@ -8,14 +8,19 @@ void UpperCase(char str[]);
 void LowerCase(char str[]);
 void Capitalize(char str[]);
 void Shrink(char str[]);
-void isPalindrome(char str[], const int n);
+bool isPalindrome(char str[]);
+void RemoveSpaces(char str[]);
+bool isNumber(char str[]);
+int StrToInt(char str[]);
+int culk(char str[]);
+
 
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	//char str[] = {'H','e','l', 'l','o', '\0'}; //инициализация строки поэлементно. "\0" обозначает конец строки.
-	/*char str[] = "Hello";						 
+	/*char str[] = "Hello";
 	cout << str << endl;
 	cout << sizeof(str)<< endl;// Размер строки. */
 	int a = 0;
@@ -27,16 +32,21 @@ void main()
 	cout << str << endl;
 	StrLen(str);			//считает количество символов
 	cout << StrLen(str) << endl;
-	UpperCase(str);			// Переводит все в верхний регистр
+	/*UpperCase(str);			// Переводит все в верхний регистр
 	cout << str << endl;
 	LowerCase(str);			//переводит в нижний регистр
 	cout << str << endl;
 	Capitalize(str);		//Преобразует все первые буквы слов в верхний регистр
 	cout << str << endl;
 	Shrink(str);			//Убирает лишние пробелы
-	cout << str << endl;
-	isPalindrome(str,n);	//Определяет,является ли строка палиндромом-
-	cout << str << endl;
+	cout << str << endl;*/
+	cout << isPalindrome(str) << endl;	//Определяет,является ли строка палиндромом-
+
+	cout << isNumber(str) << endl;
+
+	int num = StrToInt(str);
+	cout << num << endl;
+
 }
 
 void InputLine(char str[], const int n)
@@ -57,7 +67,7 @@ void UpperCase(char str[])//Из нижнего регистра в верхний
 {
 	for (int i = 0; str[i]; i++)
 	{
-		if(str[i]>='a' && str[i]<='z' || str[i]>='а' && str[i] <= 'я')str[i] -= 32;
+		if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'а' && str[i] <= 'я')str[i] -= 32;
 	}
 }
 void LowerCase(char str[])
@@ -73,8 +83,8 @@ void Capitalize(char str[])
 	if (str[0] >= 'a' && str[0] <= 'z' || str[0] >= 'а' && str[0] <= 'я')str[0] -= 32;
 	for (int i = 0; str[i]; i++)
 	{
-		if((str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'а' && str[i] <= 'я') &&
-			str[i-1]== ' ')str[i] -= 32;
+		if ((str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'а' && str[i] <= 'я') &&
+			str[i - 1] == ' ')str[i] -= 32;
 	}
 	//мой вариант
 	/*
@@ -91,16 +101,16 @@ void Capitalize(char str[])
 	}*/
 }
 void Shrink(char str[])
-{	
+{
 	//вариант учителя
 	for (int i = 0; str[i]; i++)
 	{
-		for(int i=0;str[i];i++)
+		for (int i = 0; str[i]; i++)
 
-		while(str[i] == ' '&& str[i + 1] == ' ')
-		{
-			for (int j = i + 1; str[i]; i++)str[i] = str[i + 1];
-		}
+			while (str[i] == ' '&& str[i + 1] == ' ')
+			{
+				for (int j = i + 1; str[i]; i++)str[i] = str[i + 1];
+			}
 	}
 	//другой вариант
 		/*for (int i = 0; str[i]; i++)
@@ -115,7 +125,63 @@ void Shrink(char str[])
 			}
 		}*/
 }
-void isPalindrome(char str[], const int n)
+bool isPalindrome(char str[])
 {
+	/*int n = StrLen(str);
+	char* buffer = new char[n+1] {};
+	for (int i = 0; i < n; i++)buffer[i] = str[i];
+	LowerCase(buffer);
+	RemoveSpaces(buffer);
+	n = StrLen(buffer);
+	for (int i = 0; i < n/2; i++)
+	{
+		if (buffer[i] != str[n - 1 - i])return false;
+
+	}
+	return true;*/
+	int n = StrLen(str);
+	for (int i = 0; i < n / 2; i++)
+	{
+		if (str[i] == ' ')i++, n++;
+		if (str[n - 1 - i] == ' ')n--;
+		if (str[i] != str[n - 1 - i])
+		{
+			if (str[i] + 32 != str[n - 1 - i] && str[i] != str[n - i - 1] + 32)return false;
+		}
+	}
+	return true;
+}
+void RemoveSpaces(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == ' ')
+		{
+			for (int j = 0; str[j]; j++)str[j] = str[j + 1];
+		}
+	}
+}
+
+bool isNumber(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (!(str[i] > '0' && str[i] <= '9'))return false;
+	}
+	return true;
+}
+
+int StrToInt(char str[])
+{
+	int num = 0;
+	if (isNumber(str))
+	{
+		for (int i=0;str[i];i++)
+		{
+			num *= 10;
+			num += str[i]-48;
+		}
+	}
+	return num;
 
 }
