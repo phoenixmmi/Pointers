559301f408d269af12624407dc78de17afd5f5b9
Student <Student@201-4>
2019-12-04 20:47:52 +0200

    FunctionsForString

---
+++ NullTerminatedLines/StandardStrFunctions/main.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 256;
+	/*char str[n] = {};
+	std::cout << "¬ведите строку: ";
+    std::cin.getline(str, n);
+	std::cout << str << std::endl;
+	std::cout << strlen(str) << std::endl;
+
+	char buffer[n] = {};
+	strncpy(buffer, str,3);
+	std::cout << buffer << std::endl;
+	strcat()  //  выполн€ет конкатенацию строк.
+	*/
+	
+	/*char str1[n] = "Hello";
+	char str2[n] = "World";
+	strncat(str1, str2, 4);
+	std::cout << str1 << std::endl;
+*/
+	const char* words[] = 
+	{
+		"Hello",
+		"World",
+		"Computer",
+		"C++",
+		"Imagination"
+	};
+	for (int i = 0; i < sizeof(words) / sizeof(const char*); i++)
+	{
+		std::cout << words[i] << std::endl;
+	}
+	std::cout << std::endl;
+
+	
+
+	for (int i = 0; i < sizeof(words) / sizeof(const char*); i++)
+	{
+		for (int j = 0;j < i; j++)
+		{
+			
+		}
+	}
+}
--- NullTerminatedLines/numericSystems/main.cpp
+++ NullTerminatedLines/numericSystems/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-using namespace std;
+
 
 char* dec_to_bin(int dec);
 int bin_to_dec(char* binary);
 bool isBin(char str[]);
+bool is_hex_number(const char* str);
+int hex_to_dec(const char* hex);
 
 void main()
 {
@@ -14,14 +16,20 @@ void main()
 	cout << binary << endl;
 	cout << "My binary " << binary << endl;*/
 
-
+	/*
 	const int n = 33;
 	char* binary = new char[n] {};
 	cout << "¬ведите двоичное число: "; cin >> binary;
 	cout << isBin(binary) << endl;
 	cout << bin_to_dec(binary) << endl;
-
 	delete[]binary;
+*/
+
+	const int n = 11;
+	char hex[n]{};
+	std::cout << "¬ведите hex число: "; std::cin >> hex;
+	std::cout << is_hex_number(hex) << std::endl;
+	std::cout << hex_to_dec(hex) << std::endl;
 }
 
 char* dec_to_bin(int dec)
@@ -30,7 +38,7 @@ char* dec_to_bin(int dec)
 	char* binary = new char[n] {};
 	for (int i = 0; dec; i++, dec /= 2)binary[i] = dec % 2 + 48;
 	n = strlen(binary);
-	for (int i = 0; i < n / 2; i++)swap(binary[i], binary[n - 1 - i]);
+	for (int i = 0; i < n / 2; i++)std::swap(binary[i], binary[n - 1 - i]);
 
 	/*for (n--; n >= 0; n--)
 	{
@@ -38,7 +46,7 @@ char* dec_to_bin(int dec)
 		if (n % 8 == 0)cout << " ";
 		if (n % 4 == 0)cout << " ";
 	}*/
-	cout << endl;
+	std::cout << std::endl;
 	return binary;
 }
 
@@ -55,13 +63,12 @@ int bin_to_dec(char* binary)
 {
 	int dec = 0;
 	int pow = 1;
-	for (int i = strlen(binary)-1; i >= 0; i--)
+	for (int i = strlen(binary) - 1; i >= 0; i--)
 	{
 		if (binary[i] == '1')dec += pow;
 		pow *= 2;
 	}
 	return dec;
-
 	//var1
 	/*int sum = 0;
 	int total=0;
@@ -76,5 +83,38 @@ int bin_to_dec(char* binary)
 	}
 	return sum;
 	*/
+}
 
+bool is_hex_number(const char* str)
+{
+	int i = 0;
+	if (str[0] == '0' && (str[1] == 'x' || str[1] == 'X'))i = 2;
+	if (i == 2 && str[2] == 0)return false;
+	for (; str[i]; i++)
+	{
+		if (
+			!(str[i] >= '0' && str[i] <= '9') &&
+			!(str[i] >= 'a' && str[i] <= 'f') &&
+			!(str[i] >= 'A' && str[i] <= 'F')
+			)
+			return false;
+	}
+	return true;
+}
+
+int hex_to_dec(const char* hex)
+{
+	if (!is_hex_number(hex))return 0;
+	int decimal = 0;// дес€тичное число( конечный результат) 
+	int power = 1;// весовой коеффициент разр€да(16^i)
+	for (int i = hex[1] == 'x' || hex[1] == 'X' ? 2 : 0; hex[i]; i++)
+	{
+		decimal *= 16;
+		if (hex[i] >= '0' && hex[i] <= '9')decimal += hex[i] - '0';
+		else if (hex[i] >= 'A' && hex[i] <= 'F')decimal += hex[i] - 'A' + 10;
+		else if (hex[i] >= 'a' && hex[i] <= 'f')decimal += hex[i] - 'a' + 10;
+		else throw 0;
+	}
+
+	return decimal;
 }
